5bcd3271b53246b62f8a4f822523d195d9e55b40
AndreasFahey <34457372+AndreasFahey@users.noreply.github.com>
2018-05-02 20:21:49 +0100

    Fifth Commit
    Iron Man 2 Page

---
+++ iron-man-two/iron-man-two.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { IronManTwoPage } from './iron-man-two';
+
+@NgModule({
+  declarations: [
+    IronManTwoPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(IronManTwoPage),
+  ],
+})
+export class IronManTwoPageModule {}
