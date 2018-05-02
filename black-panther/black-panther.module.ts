ca825794fd8c3eb1ef22455145ac596699176eaa
AndreasFahey <34457372+AndreasFahey@users.noreply.github.com>
2018-05-03 00:54:54 +0100

    Fifth Commit
    Black Panther

---
+++ black-panther/black-panther.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { BlackPantherPage } from './black-panther';
+
+@NgModule({
+  declarations: [
+    BlackPantherPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(BlackPantherPage),
+  ],
+})
+export class BlackPantherPageModule {}
